44fc4ad566229a4deee80d72bfee2742677c4422
Cameron Eldridge <cameldridge@gmail.com>
2021-01-06 00:02:12 -0500

    Brute force 19, but it is too slow as expected

--- 19/1/main.hs
+++ 19/1/main.hs
@@ -1,56 +1,57 @@
-{-# LANGUAGE LambdaCase, BlockArguments, ViewPatterns #-}
+{-# LANGUAGE LambdaCase, BlockArguments, ViewPatterns, TupleSections #-}
 import Control.Monad.State.Lazy
 import Control.Monad.Except
 import Control.Monad.Trans.Maybe
+import Data.Bifunctor
 import Data.Functor
 import Data.Char
 import Data.List
-import Data.Map (Map)
+import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Maybe
 
+type Assoc k v = [(k, v)]
+
 data Expansion = Terminal Char | NonTerminal [Int] deriving (Show)
-type Grammar = Map Int [Expansion]
-type NFA = Map (Int, Char) [Int]
-newtype Parser t = Parser (State (Grammar, [Int], String) t)
-
-grammar :: Monad m => StateT [String] m Grammar
-grammar = do
-    line <- state (fromJust . uncons)
-    if null line then return Map.empty else
-        let (from, to) = rule line in do
-            Map.insert from to <$> grammar
-
-number :: StateT String Maybe Int
-number = recover do
-    str <- state $ span isDigit
-    if null str
-        then throwError ()
-        else return $ read str
+type Grammar = Assoc Int Expansion
 
 recover :: StateT String Maybe t -> StateT String Maybe t
 recover f = do
     state <- get
     catchError f (\_ -> put state >> throwError ())
 
+(<|>) :: StateT String Maybe a -> StateT String Maybe a -> StateT String Maybe a
+(<|>) f g = catchError (recover f) (const g)
+
+grammar :: Monad m => StateT [String] m Grammar
+grammar = do
+    line <- state (fromJust . uncons)
+    if null line then return [] else
+        let (from, to) = rule line in do
+            (++) ((from, ) <$> to) <$> grammar
+
+number :: StateT String Maybe Int
+number = do
+    str <- state $ span isDigit
+    if null str
+        then throwError ()
+        else return $ read str
+
 terminal :: StateT String Maybe Char
-terminal = recover do
+terminal = do
     gets (stripPrefix "\"") >>= lift >>= put
     c <- state (fromJust . uncons)
     gets (stripPrefix "\"") >>= lift >>= put
     return c
 
 separated :: String -> StateT String Maybe t -> StateT String Maybe [t]
-separated separator parser = recover do
+separated separator parser = do
     lhs <- parser
     rhs <- recover (exactly separator *> separated separator parser) <|> return []
     return $ lhs : rhs
 
 exactly :: String -> StateT String Maybe ()
 exactly prefix = recover $ gets (stripPrefix prefix) >>= lift >>= put
-
-(<|>) :: MonadError e m => m a -> m a -> m a
-(<|>) f g = catchError f (const g)
 
 expansion :: StateT String Maybe Expansion
 expansion
@@ -60,22 +61,23 @@ expansion
 rule :: String -> (Int, [Expansion])
 rule = fromJust . evalStateT ((,) <$> number <*> (exactly ": " *> separated " | " expansion))
 
-partitionMap :: Traversable t => (a -> Either b c) -> t a -> (t b, t c)
-partitionMap f t =
-
-type Transition = (Int, Char, Int)
-
-nfa :: Grammar -> NFA
-nfa g = nfa_ Map.empty g
+valid :: Grammar -> [String]
+valid = (! 0) . valid_ Map.empty
     where
-        nfa_ :: NFA -> Grammar -> NFA
-        nfa_ n (null -> True) = n
-        nfa_ n g = let (rest, transitions) = partitionMap (connects n) <$> assocs g in
-            nfa_ n rest
+        valid_ :: Map Int [String] -> Grammar -> Map Int [String]
+        valid_ s [] = s
+        valid_ s g = let (now, later) = partition (ready s) g in
+            valid_ (foldl' add s now) later
 
-        connects :: NFA -> Int -> Expansion -> Maybe (Either (Int, [Expansion]) Transition)
-        connects n k (Terminal c) = Just . Right (-1, c, k)
-        connects n k v@(NonTerminal s) = find (elem s . snd) (assocs n) <&> \(i, c, _) -> Right (s, c, k)
+        ready :: Map Int a -> (Int, Expansion) -> Bool
+        ready _ (_, Terminal _) = True
+        ready s (_, NonTerminal xs) = all (`Map.member` s) xs
+
+        prev = Map.findWithDefault []
+
+        add :: Map Int [String] -> (Int, Expansion) -> Map Int [String]
+        add s (i, NonTerminal xs) = Map.insert i (prev i s `union` foldl1' (\l r -> [ll ++ rr | ll <- l, rr <- r]) ((s !) <$> xs)) s
+        add s (i, Terminal c) = Map.insert i [[c]] s
 
 run :: [String] -> IO Int
 run = evalStateT run_
@@ -83,9 +85,9 @@ run = evalStateT run_
         run_ :: StateT [String] IO Int
         run_ = do
             g <- grammar
-            n <- nfa g
             ms <- get
-            error ""
+            let v = valid g in do
+                return . length $ filter (`elem` valid g) ms
 
 main :: IO ()
 main = getContents >>= run . lines >>= print
