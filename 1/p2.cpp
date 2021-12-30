73cf761f45084ca87a4ad53761e9612a2ab838f7
Cameron Eldridge <cameldridge@gmail.com>
2021-12-29 23:50:35 -0500

    Day 1 and 2

---
+++ 1/p2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream input("input");
+    int total = 0;
+    int a = 0, b = 0, c = 0, d = 0;
+    int rownumber = 0;
+
+    while (input >> d) {
+        if (rownumber >= 3) {
+            if (a + b + c < b + c + d) {
+                total++;
+            }
+        }
+        rownumber++;
+        a = b;
+        b = c;
+        c = d;
+    }
+
+    cout << total << endl;
+    return 0;
+}
