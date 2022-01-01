67006bd6ac40fd681ad454ae44242d67a38d26d4
Cameron Eldridge <cameldridge@gmail.com>
2022-01-01 16:11:25 -0500

    Day 3 but p2 is not working and I don't know why

---
+++ 3/p2.rb
@@ -0,0 +1,23 @@
+lines = File.readlines("input").map { |l| l.chomp }
+
+def count lines, i
+    return lines
+        .filter { |line| line.chars[i] == "1" }
+        .length
+end
+
+oxygen = lines
+12.times do |i|
+    c = count(oxygen, i) >= oxygen.length / 2 ? "1" : "0"
+    oxygen = oxygen.filter { |line| line[i] == c }
+    break if oxygen.length == 1
+end
+
+carbon = lines
+12.times do |i|
+    c = count(carbon, i) >= carbon.length / 2 ? "0" : "1"
+    carbon = carbon.filter { |line| line[i] == c }
+    break if carbon.length == 1
+end
+
+puts(oxygen.first.to_i(2) * carbon.first.to_i(2))
