32f9528888bb40438347975190db46ecd23f06f8
Cameron Eldridge <cameldridge@gmail.com>
2022-12-03 21:32:26 -0500

    day 1 in bash, to get it over with before it's too late

---
+++ 01/p2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+top1=0
+top2=0
+top3=0
+
+current=0
+
+while read -r line; do
+    if [ -z "$line" ]; then
+        if (( $current > $top1 )); then
+            temp=$current
+            current=$top1
+            top1=$temp
+        fi
+        if (( $current > $top2 )); then
+            temp=$current
+            current=$top2
+            top2=$temp
+        fi
+        if (( $current > $top3 )); then
+            temp=$current
+            current=$top3
+            top3=$temp
+        fi
+        current=0
+        continue
+    fi
+    current=$(($current+$line))
+done
+
+if (( $current > $top1 )); then
+    temp=$current
+    current=$top1
+    top1=$temp
+fi
+if (( $current > $top2 )); then
+    temp=$current
+    current=$top2
+    top2=$temp
+fi
+if (( $current > $top3 )); then
+    temp=$current
+    current=$top3
+    top3=$temp
+fi
+
+echo $top1 + $top2 + $top3 = $((top1 + top2 + top3))
