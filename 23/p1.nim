618321f1ec4e2250b41cb63386cd1fe701f0ff8e
Cameron Eldridge <cameldridge@gmail.com>
2022-12-23 14:14:28 -0500

    day 23 in nim, because i am really grasping for choices but nim was actually kind of cool

---
+++ 23/p1.nim
@@ -0,0 +1,84 @@
+import std/[enumerate,sets,sugar,sequtils,tables]
+
+type Elf = tuple[x: int, y: int]
+
+iterator readelves(): Elf =
+    var y = 0
+    var line = ""
+    while readLine(stdin, line):
+        for x, ch in enumerate(line):
+            if ch == '#':
+                yield (x, y)
+        inc(y)
+
+var elves = readelves().toSeq().toHashSet()
+
+type Direction = enum n, s, w, e, ne, se, nw, sw
+
+type Check = tuple[proposed: Direction, checks: array[0..2, Direction]]
+type State = array[0..3, Check]
+
+var state: State = [
+    (proposed: n, checks: [n, ne, nw]),
+    (proposed: s, checks: [s, se, sw]),
+    (proposed: w, checks: [w, nw, sw]),
+    (proposed: e, checks: [e, ne, se]),
+]
+
+proc move(elf: Elf, direction: Direction): Elf =
+    case direction
+        of n: (elf.x, elf.y - 1)
+        of ne: (elf.x + 1, elf.y - 1)
+        of nw: (elf.x - 1, elf.y - 1)
+        of s: (elf.x, elf.y + 1)
+        of se: (elf.x + 1, elf.y + 1)
+        of sw: (elf.x - 1, elf.y + 1)
+        of e: (elf.x + 1, elf.y)
+        of w: (elf.x - 1, elf.y)
+
+proc propose(state: State, elves: HashSet[Elf], elf: Elf): Elf =
+    let hasneighbour = toSeq(n..sw)
+        .map(d => move(elf, d))
+        .any(e => e in elves)
+    if not hasneighbour:
+        return elf
+
+    for (proposed, checks) in state:
+        let conflicts = checks
+            .map(d => move(elf, d))
+            .any(e => e in elves)
+        if not conflicts:
+            return move(elf, proposed)
+    return elf
+
+for i in 0..9:
+    let proposals = collect():
+        for elf in elves:
+            (elf, propose(state, elves, elf))
+
+    var counted = initTable[Elf, int]()
+    for (elf, proposal) in proposals:
+        if proposal in counted:
+            inc(counted[proposal])
+        else:
+            counted[proposal] = 1
+
+    iterator updated(): Elf =
+        for (elf, proposal) in proposals:
+            if counted[proposal] == 1:
+                yield proposal
+            else:
+                yield elf
+    elves = updated().toSeq().toHashSet()
+    # ever seen a language where this assigns to the array one index at a time?
+    # state = [state[1], state[2], state[3], state[0]]
+    let temp = state[0]
+    state = [state[1], state[2], state[3], temp]
+
+
+let minx = toSeq(elves).map(e => e.x).foldl(min(a, b))
+let miny = toSeq(elves).map(e => e.y).foldl(min(a, b))
+let maxx = toSeq(elves).map(e => e.x).foldl(max(a, b))
+let maxy = toSeq(elves).map(e => e.y).foldl(max(a, b))
+
+echo((maxx - minx + 1) * (maxy - miny + 1) - elves.len())
