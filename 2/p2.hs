068f33d169c5dfee20e16ea75990a49bbde2b82d
Cameron Eldridge <cameldridge@gmail.com>
2024-12-04 18:23:42 -0500

    shorter for 2/2

--- 2/p2.hs
+++ 2/p2.hs
@@ -1,15 +1,14 @@
 absdiff a b = abs (a - b)
 
-isSafe xs = isSafe_ (<) xs || isSafe_ (>) xs || isLessSafe_ (<) (drop 1 xs) || isLessSafe_ (>) (drop 1 xs)
+isSafe xs = isSafe_ (<) xs || isSafe_ (>) xs
   where
     isSafe_ cmp [] = True
     isSafe_ cmp [_] = True
-    isSafe_ cmp (a : b : xs) = (cmp a b && absdiff a b <= 3 && isSafe_ cmp (b : xs)) || isLessSafe_ cmp (a : xs)
+    isSafe_ cmp (a : b : xs) = cmp a b && absdiff a b <= 3 && isSafe_ cmp (b : xs)
 
-    isLessSafe_ cmp [] = True
-    isLessSafe_ cmp [_] = True
-    isLessSafe_ cmp (a : b : xs) = cmp a b && absdiff a b <= 3 && isLessSafe_ cmp (b : xs)
+omissions [] = []
+omissions (x : xs) = xs : ((x :) <$> omissions xs)
 
-answer = length . filter isSafe . fmap (fmap read . words) . lines
+answer = length . filter (any isSafe . omissions) . fmap (fmap read . words) . lines
 
 main = getContents >>= print . answer
