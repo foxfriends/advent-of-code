a9d3c1ea461a1b29a4fbbb1689edbaf47534d085
Cameron Eldridge <cameldridge@gmail.com>
2024-12-08 11:12:06 -0500

    improve day 4 with array instead of list for indexing

--- 4/p1.hs
+++ 4/p1.hs
@@ -1,17 +1,22 @@
-import Control.Monad
 import Control.Monad.Reader
-import Data.List
+import Data.Array.IArray
 import Data.Maybe
 
-at x y = reader ((!? x) <=< (!? y))
+readGrid :: String -> Array (Int, Int) Char
+readGrid contents = listArray ((0, 0), (width - 1, height - 1)) $ concat input
+  where
+    input = lines contents
+    width = length (input !! 0)
+    height = length input
+
+at x y = reader (flip (!?) (x, y))
 
 spokesAt x y = sequence [readWord x y | y <- [-1 .. 1], x <- [-1 .. 1], (x, y) /= (0, 0)]
   where
     readWord dx dy = sequence <$> sequence [at (x + i * dx) (y + i * dy) | i <- [0 .. 3]]
 
-answer input = length $ filter ("XMAS" ==) $ catMaybes $ concat $ runReader (mapM (uncurry spokesAt) everywhere) grid
+answer contents = length $ filter ("XMAS" ==) $ catMaybes $ concat $ runReader (mapM (uncurry spokesAt) $ indices grid) grid
   where
-    grid = lines input
-    everywhere = [(x, y) | y <- [0 .. length grid - 1], x <- [0 .. length (grid !! y) - 1]]
+    grid = readGrid contents
 
 main = getContents >>= print . answer
--- 4/p2.hs
+++ 4/p2.hs
@@ -1,9 +1,15 @@
-import Control.Monad
 import Control.Monad.Reader
-import Data.List
+import Data.Array.IArray
 import Data.Maybe
 
-at x y = reader ((!? x) <=< (!? y))
+readGrid :: String -> Array (Int, Int) Char
+readGrid contents = listArray ((0, 0), (width - 1, height - 1)) $ concat input
+  where
+    input = lines contents
+    width = length (input !! 0)
+    height = length input
+
+at x y = reader (flip (!?) (x, y))
 
 isMas x = x == "MAS" || x == "SAM"
 
@@ -11,9 +17,8 @@ crossAt x y = sequence <$> sequence [readWord 1 1, readWord 1 (-1)]
   where
     readWord dx dy = sequence <$> sequence [at (x - dx) (y - dy), at x y, at (x + dx) (y + dy)]
 
-answer input = length $ filter (all isMas) $ catMaybes $ runReader (mapM (uncurry crossAt) everywhere) grid
+answer contents = length $ filter (all isMas) $ catMaybes $ runReader (mapM (uncurry crossAt) $ indices grid) grid
   where
-    grid = lines input
-    everywhere = [(x, y) | y <- [0 .. length grid - 1], x <- [0 .. length (grid !! y) - 1]]
+    grid = readGrid contents
 
 main = getContents >>= print . answer
