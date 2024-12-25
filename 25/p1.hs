33ef4925216faf67fe2adbfa31272a6399172655
Cameron Eldridge <cameldridge@gmail.com>
2024-12-25 00:46:49 -0500

    a little nicer

--- 25/p1.hs
+++ 25/p1.hs
@@ -6,6 +6,6 @@ keyslocks ("" : rest) = keyslocks rest
 keyslocks ("....." : a : b : c : d : e : "#####" : rest) = (Left $ fmap length $ filter (== '#') <$> transpose [a, b, c, d, e]) : keyslocks rest
 keyslocks ("#####" : a : b : c : d : e : "....." : rest) = (Right $ fmap length $ filter (== '#') <$> transpose [a, b, c, d, e]) : keyslocks rest
 
-answer = length . filter (all (\(a, b) -> a + b <= 5) . uncurry zip) . uncurry (liftA2 (,)) . partitionEithers . keyslocks . lines
+answer = length . filter (all (<= 5) . uncurry (zipWith (+))) . uncurry (liftA2 (,)) . partitionEithers . keyslocks . lines
 
 main = getContents >>= print . answer
