8e22467b41d08a1001a970de24ae26f7cad5af67
Cameron Eldridge <cameldridge@gmail.com>
2024-12-28 00:57:08 -0500

    day 15 rework

--- 15/p1.hs
+++ 15/p1.hs
@@ -1,29 +1,29 @@
-import qualified Data.Map as Map
-import Control.Monad.State.Lazy
+import Control.Monad.State
 import Data.Map (Map)
+import Data.Map qualified as Map
+import Text.Parsec
 
-split :: Eq a => a -> [a] -> [[a]]
-split x ys = splitInner x [] ys
-    where
-        splitInner _ [] [] = []
-        splitInner _ r [] = [reverse r]
-        splitInner x r (y : ys)
-            | x == y = reverse r : splitInner x [] ys
-            | otherwise = splitInner x (y : r) ys
+int :: Parsec String u Int
+int = read <$> many digit
+
+parseInput = int `sepBy` char ','
 
 run :: Int -> [Int] -> Int
-run n st = evalState (run_ n) (Map.fromList $ zip (init st) [1..])
-    where
-        run_ :: Int -> State (Map Int Int) Int
-        run_ n | n <= length st = return $ st !! (n - 1)
-        run_ i = do
-            prev <- run_ $ i - 1
-            before <- gets (Map.lookup prev)
-            let next = maybe 0 (i - 1 -) before in do
-                modify (Map.insert prev (i - 1))
-                return next
+run n st = evalState (run_ n) (Map.fromList $ zip (init st) [1 ..])
+  where
+    run_ :: Int -> Control.Monad.State.State (Map Int Int) Int
+    run_ n | n <= length st = return $ st !! (n - 1)
+    run_ i = do
+      prev <- run_ $ i - 1
+      before <- gets (Map.lookup prev)
+      let next = maybe 0 (i - 1 -) before
+       in do
+            modify (Map.insert prev (i - 1))
+            return next
+
+answer contents = run 2020 input
+  where
+    Right input = parse parseInput "" contents
 
 main :: IO ()
-main = do
-    numbers <- fmap read . split ',' <$> getContents
-    print $ run 2020 numbers
+main = getContents >>= print . answer
--- 15/p2.hs
+++ 15/p2.hs
@@ -1,29 +1,28 @@
-import qualified Data.Map as Map
-import Control.Monad.State.Lazy
-import Data.Map (Map)
+import Control.Monad.State
+import Data.IntMap.Strict (IntMap, (!?))
+import Data.IntMap.Strict qualified as IntMap
+import Text.Parsec
 
-split :: Eq a => a -> [a] -> [[a]]
-split x ys = splitInner x [] ys
-    where
-        splitInner _ [] [] = []
-        splitInner _ r [] = [reverse r]
-        splitInner x r (y : ys)
-            | x == y = reverse r : splitInner x [] ys
-            | otherwise = splitInner x (y : r) ys
+int :: Parsec String u Int
+int = read <$> many digit
+
+parseInput = int `sepBy` char ','
 
 run :: Int -> [Int] -> Int
-run n st = evalState (run_ n) (Map.fromList $ zip (init st) [1..])
-    where
-        run_ :: Int -> State (Map Int Int) Int
-        run_ n | n <= length st = return $ st !! (n - 1)
-        run_ i = do
-            prev <- run_ $ i - 1
-            before <- gets (Map.lookup prev)
-            let next = maybe 0 (i - 1 -) before in do
-                modify (Map.insert prev (i - 1))
-                return next
+run target st = run_ (length st + 1) 0 (IntMap.fromList $ zip st [1 ..])
+  where
+    run_ :: Int -> Int -> IntMap Int -> Int
+    run_ i n _ | i == target = n
+    run_ i n map =
+      let prev = map !? n
+          map' = IntMap.insert n i map
+       in case prev of
+            Nothing -> run_ (i + 1) 0 map'
+            Just time -> run_ (i + 1) (i - time) map'
+
+answer contents = run 30000000 input
+  where
+    Right input = parse parseInput "" contents
 
 main :: IO ()
-main = do
-    numbers <- fmap read . split ',' <$> getContents
-    print $ run 2020 numbers
+main = getContents >>= print . answer
