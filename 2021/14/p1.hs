e8ef06e1bb1c3b9295b4cffc1d208237fa6efc72
Cameron Eldridge <cameldridge@gmail.com>
2025-01-02 22:45:44 -0500

    2021:14

---
+++ 2021/14/p1.hs
@@ -0,0 +1,35 @@
+import Control.Arrow
+import Text.Parsec
+import Data.List
+import Data.Map qualified as Map
+
+initialize ps = Map.unionsWith (+) $ fmap (flip Map.singleton 1) $ ps `zip` tail ps
+
+rule = do
+  a:b:_ <- count 2 letter
+  string " -> "
+  to <- letter
+  return $ Map.singleton (a, b) [(a, to), (to, b)]
+
+input = do
+  initial <- many letter
+  count 2 newline
+  rules <- rule `endBy` newline
+  return (initial, Map.unions rules)
+
+step rules state = Map.unionsWith (+) $ do
+  (k, v) <- Map.assocs state
+  k2 <- rules Map.! k
+  return $ Map.singleton k2 v
+
+answer contents =
+  uncurry (-)
+    $ maximum &&& minimum
+    $ Map.unionWith (+) (Map.singleton (last initial) 1)
+    $ Map.mapKeysWith (+) fst
+    $ (!! 10)
+    $ iterate (step rules)
+    $ initialize initial
+  where Right (initial, rules) = parse input "" contents
+
+main = getContents >>= print . answer
