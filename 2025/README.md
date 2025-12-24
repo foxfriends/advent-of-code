8236382cf4e9cf8b20e9fdc42c2c98e8b11eaae6
Cameron Eldridge <cameldridge@gmail.com>
2025-12-23 21:07:20 -0500

    start a script to run all and record stats

---
+++ 2025/README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2025
+
+Back to (mostly) Trilogy this time, depending on performance needs; Trilogy (post-rewrite) was just barely functioning this time around.
---
+++ all.rb
@@ -0,0 +1,238 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+def which cmd
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each do |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable?(exe) && !File.directory?(exe)
+    end
+  end
+  nil
+end
+
+def measure
+    before = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+    yield
+    after = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+    after - before
+end
+
+def report label
+    answer = nil
+    time = measure do
+        answer = yield
+    end
+    return :program_error unless $?.success?
+    answer = answer.chomp
+    puts "\t#{label}: #{answer} in #{(time / 10000).to_f / 100}ms"
+    return {
+        duration_ns: time,
+        answer: answer
+    }
+end
+
+module Rust
+    def self.available?
+        which "rustc" != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.rs"
+    end
+
+    def self.run part
+        `rustc p#{part}.rs 2> /dev/null`
+        return :rustc_error unless $?.success?
+        report "p#{part}.rs" do
+            `timeout 1m ./p#{part} < input`
+        end
+    end
+end
+
+module Haskell
+    def self.available?
+        which "ghc" != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.hs"
+    end
+
+    def self.run part
+        `ghc p#{part}.hs 2> /dev/null`
+        return :ghc_error unless $?.success?
+        report "p#{part}.hs" do
+            `timeout 1m ./p#{part} < input`
+        end
+    end
+end
+
+module Trilogy
+    def self.clang
+        return "clang" if which "clang"
+        return "clang-19" if which "clang-19"
+        nil
+    end
+
+    def self.available?
+        which "trilogy" != nil and self.clang != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.tri"
+    end
+
+    def self.run part
+        `trilogy compile p#{part}.tri 2> /dev/null | #{self.clang} -O3 -o p#{part} -x ir -`
+        return :trilogy_error unless $?.success?
+        report "p#{part}.tri" do
+            `timeout 1m ./p#{part} < input`
+        end
+    end
+end
+
+module C
+    def self.clang
+        return "clang" if which "clang"
+        return "clang-19" if which "clang-19"
+        return "gcc" if which "gcc"
+        nil
+    end
+
+    def self.available?
+        self.clang != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.c"
+    end
+
+    def self.run part
+        `#{self.clang} -O3 -o p#{part} p#{part}.c`
+        return :clang_error unless $?.success?
+        report "p#{part}.c" do
+            `timeout 1m ./p#{part} < input`
+        end
+    end
+end
+
+module Cpp
+    def self.available?
+        which "g++" != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.cpp"
+    end
+
+    def self.run part
+        `g++ -std=c++2c -O3 -o p#{part} p#{part}.cpp`
+        return :gcc_error unless $?.success?
+        report "p#{part}.cpp" do
+            `timeout 1m ./p#{part} < input`
+        end
+    end
+end
+
+module Swift
+    def self.available?
+        which "swiftc" != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.swift"
+    end
+
+    def self.run part
+        `swiftc p#{part} p#{part}.swift`
+        return :swiftc_error unless $?.success?
+        report "p#{part}.swift" do
+            `timeout 1m ./p#{part} < input`
+        end
+    end
+end
+
+module Python
+    def self.python
+        return "python3" if which "python3"
+        return "python" if which "python"
+        return nil
+    end
+    
+    def self.available?
+        self.python != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.py"
+    end
+
+    def self.run part
+        report "p#{part}.py" do
+            `timeout 1m #{self.python} p#{part}.py < input`
+        end
+    end
+end
+
+module Ruby
+    def self.available?
+        which "ruby" != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.rb"
+    end
+
+    def self.run part
+        report "p#{part}.rb" do
+            `timeout 1m ruby p#{part}.rb < input`
+        end
+    end
+end
+
+module TypeScript
+    def self.available?
+        which "deno" != nil
+    end
+
+    def self.implemented? part
+        File.exist? "p#{part}.ts"
+    end
+
+    def self.run part
+        report "p#{part}.ts" do
+            `timeout 1m deno p#{part}.ts < input`
+        end
+    end
+end
+
+languages = [Haskell, Rust, Trilogy, C, Cpp, Swift, Python, Ruby, TypeScript]
+    .filter { |lang| lang.available? }
+
+root = Dir.pwd
+report = {}
+
+for year in `ls .`.split.filter { |x| /^\d+$/ =~ x } do
+    report[year] = {}
+    for day in 1..25 do
+        next unless Dir.exist? "#{year}/#{day}"
+        report[year][day] = {}
+        puts "year #{year} day #{day}"
+        `just get #{day} #{year} 2> /dev/null`
+
+        Dir.chdir "#{year}/#{day}"
+        for part in ['1', '2'] do
+            report[year][day]["p#{part}"] = {}
+            for lang in languages do
+                next unless lang.implemented? part
+                report[year][day]["p#{part}"][lang] = lang.run part
+            end
+        end
+        Dir.chdir root
+    end
+end
+
+File.write "report.json", JSON.pretty_generate(report)
